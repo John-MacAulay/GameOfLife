5032461ad3c80a7bb82e81825011f11c06ada44d
John-MacAulay <88356693+John-MacAulay@users.noreply.github.com>
2022-03-16 14:55:37 +1100

    This corelogic has been moved further out

--- GameOfLifeTests/CoreLogicTests.cs
+++ GameOfLifeTests/CoreLogicTests.cs
@@ -6,15 +6,7 @@ namespace GameOfLifeTests
 {
     public class CoreLogicTests
     {
-        [Fact]
-        public void GivenGetWorldManually_ItWillReturnAnEmptyNewWorldAccordingToInputParameters()
-        {
-            // Arrange 
-            var testOutput = new TestOutput();
-            var testInput = new TestInput(new []{"10","15"});
-            var coreLogic = new CoreLogic(testOutput, testInput);
-       
-        }
+        
         
     }
 }
